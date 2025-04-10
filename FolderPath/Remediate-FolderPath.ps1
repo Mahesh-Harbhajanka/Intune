4856d98d58cbf0d2666f77211e7c01a653350f1c
Mahesh-Harbhajanka <mahesh.harbhajanka@hotmail.com>
2025-04-10 16:12:35 +1000

    Delete FolderPath directory

--- FolderPath/Remediate-FolderPath.ps1
+++
@@ -1,9 +0,0 @@
-﻿<#
-Version: 1.0
-Author: 
-Mahesh Harbhajanka
-#> 
-
-Write-Output "Remediating missing folder"
-New-Item -Name "Mahesh" -Path "c:\" -Itemtype Directory
-Write-Output "Remediation completed for folder Mahesh"
